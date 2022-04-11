32466dace5846227d95f27123358d5c4fb9ec9dd
Lily Pham <lilpham@ucsc.edu>
2022-04-11 03:20:13 -0700

    working moving rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -50,5 +50,22 @@ class Play extends Phaser.Scene {
         0xffffff
       )
       .setOrigin(0, 0);
+    //add rocket (p1)
+    this.p1Rocket = new Rocket(
+      this,
+      game.config.width / 2,
+      game.config.height - borderUISize - borderPadding,
+      "rocket"
+    ).setOrigin(0.5, 0);
+
+    // define keys
+    keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+    keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+  }
+  update() {
+    this.starfield.tilePositionX -= 4;
+    this.p1Rocket.update();
   }
 }
